ef8cb2423e320ee0ebdae89b6c303fe853dbd5e0
dalong982242260 <zwl19881212>
2016-11-10 19:21:37 +0800

    底部tab icon设置32

--- app/src/main/java/com/dalong/bottomtabdemo/MainActivity.java
+++ app/src/main/java/com/dalong/bottomtabdemo/MainActivity.java
@@ -1,10 +1,9 @@
 package com.dalong.bottomtabdemo;
 
-import android.graphics.Color;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
-import android.widget.Toast;
 
 import com.dalong.buttomtablayout.BottomTab;
 import com.dalong.buttomtablayout.BottomTabLayout;
@@ -13,7 +12,22 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
+/**
+ * Android官方底部Tab栏设计规范
+ *
+ * 1、推荐底部可以放置3到5个。
+ * 2、推荐选中的图标或者文字为APP的主色调，如果Tab栏本身就是彩色，推荐黑色和白色作为图标或者文字
+ * 3、选中的Tab同时显示icon和text。
+ *    如果只有三个Tab，无论选中未选中，一直显示icon和文字。
+ *    如果有四到五个Tab，选中的Tab显示文字和icon，未选中的Tab只显示icon
+ * 4、文字要求言简意赅
+ * 5、Bottom navigation bars的高度推荐为56dp，icon的尺寸为24*24，这种Google一般推荐使用8的倍数。选中tab的字体大小为14sp,未选中为12sp
+ *
+ * Created by zhouweilong on 2016/11/10.
+ */
 public class MainActivity extends AppCompatActivity {
+
+    public final String TAG=MainActivity.class.getSimpleName();
 
     private String[] mTabNames = {"首页", "消息", "搜索", "我的"};
 
@@ -64,7 +78,9 @@ public class MainActivity extends AppCompatActivity {
      */
     private int mSelectColor = R.color.SelectColor;
 
+    //底部控件
     private BottomTabLayout mBottomTabLayout;
+    //tab数据
     private List<BottomTab> mBottomTabs = new ArrayList<>();
 
     @Override
@@ -79,12 +95,12 @@ public class MainActivity extends AppCompatActivity {
         mBottomTabLayout.setOnTabChangeListener(new BottomTabLayout.OnTabChangeListener() {
             @Override
             public void onTabSelect(int position) {
-                Toast.makeText(MainActivity.this, "您选择了"+position, Toast.LENGTH_SHORT).show();
+                Log.v(TAG, "您选择了"+position);
             }
 
             @Override
             public void onTabSelected(int position) {
-                Toast.makeText(MainActivity.this, "您已经选择了"+position, Toast.LENGTH_SHORT).show();
+                Log.v(TAG, "您已经选择了"+position);
             }
         });
     }
--- buttomtablayout/src/main/java/com/dalong/buttomtablayout/BottomTabLayout.java
+++ buttomtablayout/src/main/java/com/dalong/buttomtablayout/BottomTabLayout.java
@@ -16,12 +16,22 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
+ * Android官方底部Tab栏设计规范
+ *
+ * 1、推荐底部可以放置3到5个。
+ * 2、推荐选中的图标或者文字为APP的主色调，如果Tab栏本身就是彩色，推荐黑色和白色作为图标或者文字
+ * 3、选中的Tab同时显示icon和text。
+ *    如果只有三个Tab，无论选中未选中，一直显示icon和文字。
+ *    如果有四到五个Tab，选中的Tab显示文字和icon，未选中的Tab只显示icon
+ * 4、文字要求言简意赅
+ * 5、Bottom navigation bars的高度推荐为56dp，icon的尺寸为24*24，这种Google一般推荐使用8的倍数。选中tab的字体大小为14sp,未选中为12sp
+ *
  * 底部tab切换布局
  * Created by zhouweilong on 2016/11/10.
  */
 
 public class BottomTabLayout extends LinearLayout {
-
+    //上下文
     private  Context mContext;
     //底部tab数据
     private List<BottomTab> mBottomTabs=new ArrayList<>();
